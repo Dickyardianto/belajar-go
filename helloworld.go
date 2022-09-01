651b285225136998412eeb65cccf2a58457febaf
Dickyardianto <dikiardianto19@gmail.com>
2022-09-01 14:59:04 +0700

    Belajar golang tipe data integer

---
+++ helloworld.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Word")
+}
+
+/* tipe data number 
+- integer 
+- floating point
+tipe data berbentuk case sensitif
+
+integer nilai minimum ada negatif, 
+unsign integer tidak ada negatif dalam nilai minimum
+floating point = ada komanya
