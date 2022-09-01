602d01cc7021f6e133c73b644abded4f42fbea00
Dickyardianto <dikiardianto19@gmail.com>
2022-09-01 15:18:12 +0700

    add tipe data string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("Dicky"))
+	fmt.Println("Dicky Ardianto"[0])
+	fmt.Println("Ardianto Dicky"[1])
+
+	fmt.Println("Dicky")
+	fmt.Println("Dicky Ardianto")
+	fmt.Println("Ardianto Dicky")
+}
